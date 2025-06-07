d4223969fc524ebc1f566b29227cd6df98e3b94d
ChengJin-Monash <cjin0018@student.monash.edu>
2025-06-07 11:24:31 +1000

    tests  6.7.3

---
+++ birdtag-system/birdtag-system-6.6.3/tests/conftest.py
@@ -0,0 +1,28 @@
+import pytest
+import boto3
+import os
+from moto import mock_dynamodb, mock_s3, mock_cognito
+
+@pytest.fixture(scope="function")
+def aws_credentials():
+    """Mocked AWS Credentials for moto."""
+    os.environ["AWS_ACCESS_KEY_ID"] = "testing"
+    os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
+    os.environ["AWS_SECURITY_TOKEN"] = "testing"
+    os.environ["AWS_SESSION_TOKEN"] = "testing"
+    os.environ["AWS_DEFAULT_REGION"] = "us-east-1"
+
+@pytest.fixture(scope="function")
+def dynamodb(aws_credentials):
+    with mock_dynamodb():
+        yield boto3.resource("dynamodb")
+
+@pytest.fixture(scope="function")
+def s3(aws_credentials):
+    with mock_s3():
+        yield boto3.client("s3")
+
+@pytest.fixture(scope="function")
+def cognito(aws_credentials):
+    with mock_cognito():
+        yield boto3.client("cognito-idp") 
